f6e9f56d890d5f37128648bc8eb7d46d7ba47eb9
Eve Cho <e.young.echo@gmail.com>
2022-08-14 23:39:58 -0400

    codys cafe is complete! comments added to my funky functions, and refactored camelCase a bit more

--- server/funky-funcs/index.js
+++ server/funky-funcs/index.js
@@ -1,45 +1,55 @@
+// returns array of items that the provided arrays all have in common
 const intersection = (...args) => {
   return args.reduce((prev, curr) => {
     return prev.filter(thing => curr.includes(thing));
   })
 }
 
+// a version of .flat() that will run rescursively and completely
+// flatten an array that has any number of nested sub-arrays
+// currently only works for numbers... but could add other type comparisons too
 // i feel like this shouldn't work but it does??????
 const flattenDeep = (arr, i = 0) => {
   // this is the recursive case...
+  // it checks if there is a sub-array present (so NOT a primitive type!!!)
   if(arr.some(thing => typeof thing !== 'number')) {
+    // will send a flattened version of that into the next call!
     return flattenDeep(arr.flat(), i++);
   }
   // and this is the base case?????
-  return arr.flat();
+  // just returns the array as is if there is no sub-array to be found
+  return arr;
 }
 
+// creates a function, given a function, that runs said function but with parameters in reverse order
 const flipArguments = (func) => {
   return (...args) => {
-    args.reverse();
-    return func(...args);
+    return func(...args.reverse());
   }
 }
 
+// inverts the keys and values of an object
 const invert = (obj) => {
+  // the keys of the object, to be the values in the new obj!
   const toBeValues = Object.keys(obj);
-  const toBeKeys = Object.values(obj);
+  // this will be our new inverted object :)
   const thing = {};
-  toBeKeys.forEach((key, ind) => {
+  // for each value of the object, it makes a key of that value-name
+  Object.values(obj).forEach((key, ind) => {
+    // ... and its value is assigned the corresponding key it belonged to
     thing[key] = toBeValues[ind];
   })
   return thing;
 }
 
+// returns an inputted string but in camelCase format (all words sans the first is capitalized and then conjoined)
+// works for any number of whitespace and any number of '_' underscores
 const camelCase = (str) => {
-  const regex = /[_*\s*]/ig
-  let cameling = str.replaceAll(regex, ' ')
-    .split(' ')
-    .filter(word => word.length > 0)
-    .map(word => word = word[0].toUpperCase() + word.slice(1))
-    .join('');
-  const final = cameling[0].toLowerCase() + cameling.slice(1);
-  return final;
+  /* by .split(), each word is its own array element, even empty strings */
+  let camel = str.replaceAll(/[_\s]/ig, ' ').split(' ').reduce((prev, currWord) => currWord.length > 0 ? prev += currWord[0].toUpperCase() + currWord.slice(1) : prev, '')
+  // reduces our array such that if the word is valid (not empty), the first letter is capitalized and concatenated to the total string of words
+  // otherwise, just returns the string of words so far as is
+  return camel[0].toLowerCase() + camel.slice(1);
 }
 
 module.exports = {
